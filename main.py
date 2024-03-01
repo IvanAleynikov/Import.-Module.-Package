7c939187c827a0a30c282553b2a62c297a9fb6a1
Ivan Aleinikov <alejnikov99.99@mail.ru>
2024-03-01 19:44:30 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from application.salary import calculate_salary
+from application.db.people import get_employees
+
+
+if __name__ == '__main__':
+    print('Основная функция')
+
+salary.calculate_salary()
+people.get_employees()
